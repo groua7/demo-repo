285a9b971b71e279056af46f8613cc3c77a4b2da
groua7 <125916194+groua7@users.noreply.github.com>
2023-11-24 08:18:55 -0800

    Create conditions.py
    python for all

---
+++ devops/conditions.py
@@ -0,0 +1,8 @@
+# if elif and else condition for real life example
+age = 20
+if age <18:
+    print("you are too young to vote.")
+elif age >= 18 and age  <= 21:
+    print("you are allowed to vote, but not old enough for alcohol cosumption.")
+else:
+    print("you are legally allowed to vote")
